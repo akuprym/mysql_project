e2d8d7e94a0a901c41cba0226dd7957c134f2db1
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-10-12 02:59:06 +0300

    select columns, cursor.fetchall()

--- mysql_project.py
+++ mysql_project.py
@@ -3,31 +3,16 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="Qwas74123",
     database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
+mycursor.execute("SELECT name, address FROM customers")
 
-mycursor.executemany(sql, val)
+myresult = mycursor.fetchall()
 
-mydb.commit() # This line is required to make the changes
+for x in myresult:
+    print(x)
 
-print(mycursor.rowcount, "records were inserted.")
