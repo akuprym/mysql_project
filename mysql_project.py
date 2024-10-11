36af0b52793cfba60c32b773bf9c77874ade9338
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-10-11 23:27:29 +0300

    show databases

--- mysql_project.py
+++ mysql_project.py
@@ -9,5 +9,10 @@ mydb = mysql.connector.connect(
 print(mydb)
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE mydatabase")
 
+# mycursor.execute("CREATE DATABASE mydatabase")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
