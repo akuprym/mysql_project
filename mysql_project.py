18053e13000e4ac487b4fdacc1bca503274cbd92
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-10-11 23:33:52 +0300

    create table

--- mysql_project.py
+++ mysql_project.py
@@ -3,16 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password=""
+    password="",
+    database="mydatabase"
 )
-
-print(mydb)
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("CREATE TABLE customers(name VARCHAR (225), address VARCHAR (225))")
 
-mycursor.execute("SHOW DATABASES")
+mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
     print(x)
