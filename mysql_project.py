0eba0e79744ae4a8e77227c9d53e28ea0564a4f8
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-10-12 00:21:05 +0300

    alter table _ add column

--- mysql_project.py
+++ mysql_project.py
@@ -3,13 +3,13 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="Qwas74123",
     database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers(name VARCHAR (225), address VARCHAR (225))")
+mycursor.execute("ALTER TABLE customers ADD COLUMN(id INT AUTO_INCREMENT PRIMARY KEY")
 
 mycursor.execute("SHOW TABLES")
 
