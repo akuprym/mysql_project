619deaf95553448ae0179bdaa75415bca6e005b4
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-10-12 23:59:19 +0300

    remove comments

--- mysql_project.py
+++ mysql_project.py
@@ -10,43 +10,42 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # - select records where address contains "way":
-# mycursor.execute("SELECT * FROM customers WHERE address LIKE '%way%'")
+mycursor.execute("SELECT * FROM customers WHERE address LIKE '%way%'")
 
 # - use placeholder %s to escape values:
-# sql = "SELECT * FROM customers WHERE address = %s"
-# adr = ("Yellow Garden 2, ")
-# mycursor.execute(sql, adr)
-# myresult = mycursor.fetchall()
+sql = "SELECT * FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2, ")
+mycursor.execute(sql, adr)
+myresult = mycursor.fetchall()
 
 # - Order ascending:
-#mycursor.execute("SELECT * FROM customers ORDER BY name")
+mycursor.execute("SELECT * FROM customers ORDER BY name")
 
 # - Order descending:
-# mycursor.execute("SELECT * FROM customers ORDER BY name DESC")
+mycursor.execute("SELECT * FROM customers ORDER BY name DESC")
 
 # - Delete:
-# mycursor.execute("DELETE FROM customers WHERE address = 'Mountain 21'")
-# mydb.commit()
+mycursor.execute("DELETE FROM customers WHERE address = 'Mountain 21'")
+mydb.commit()
 
-# print(mycursor.rowcount, "record(s) deleted")
+print(mycursor.rowcount, "record(s) deleted")
 
 # - Delete table
-# sql = "DROP TABLE IF EXISTS clients"
-# mycursor.execute(sql)
+sql = "DROP TABLE IF EXISTS clients"
+mycursor.execute(sql)
 
 # - Overwrite record:
-# sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
-#mycursor.execute(sql)
-#mydb.commit()
-#print(mycursor.rowcount, "record(s) affected")
+sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount, "record(s) affected")
 
 # - Use placeholder %s to escape values
-
-# sql = "UPDATE customers SET address = %s WHERE address = %s"
-# val = ("Valley 345", "Canyon 123")
-# mycursor.execute(sql, val)
-# mydb.commit()
-# print(mycursor.rowcount, "record(s) affected")
+sql = "UPDATE customers SET address = %s WHERE address = %s"
+val = ("Valley 345", "Canyon 123")
+mycursor.execute(sql, val)
+mydb.commit()
+print(mycursor.rowcount, "record(s) affected")
 
 # Return 5 records, start from position 3:
 sql = "SELECT * FROM customers LIMIT 5 OFFSET 2"
@@ -54,3 +53,4 @@ mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
     print(x)
+
